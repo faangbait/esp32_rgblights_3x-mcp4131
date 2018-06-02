20498b7b3736f701eaa462772dcc1d6d13a47e2d
The Right Honourable Reverend <samueljsmith@gmail.com>
2018-06-02 08:45:33 -0600

    MQTT version

--- wakeup.py
+++ wakeup.py
@@ -1,25 +1,22 @@
 import time
+import paho.mqtt.client as paho
 
-for i in range(0,255):
-	f=open("/home/bb/rgblights/blue2","w")
-	f.write('0')
-	f.close()
-	f=open("/home/bb/rgblights/red2","w")
-	f.write(str(i))
-	f.close()
-	f=open("/home/bb/rgblights/green2","w")
-	f.write(str(i/2))
-	f.close()
-	time.sleep(5)
+broker="192.168.1.200"
+port=1883
+client1=paho.Client("control1")
+client1.connect(broker,port)
 
-for i in range(0,255):
-	f=open("/home/bb/rgblights/red2","w")
-	f.write(str(255))
-	f.close()
-	f=open("/home/bb/rgblights/green2","w")
-	f.write(str(max(127,i)))
-	f.close()
-	f=open("/home/bb/rgblights/blue2","w")
-	f.write(str(i))
-	f.close()
-	time.sleep(1)
+client1.publish("blue1","127")
+for i in range(0,127):
+        client1.publish("red1",str(127-i))
+        client1.publish("green1",str(127-(i/2)))
+        time.sleep(.3)
+
+client1.publish("red1","0")
+for i in range(0,127):
+        client1.publish("green1",str(min(64,127-i)))
+        client1.publish("blue1",str(127-i))
+        time.sleep(.3)
+client1.publish("red1","0")
+client1.publish("blue1","0")
+client1.publish("green1","0")
